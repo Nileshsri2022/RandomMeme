8be435dd2a9bdc3ac2f5da55c548ea5c508bcdcf
Nilesh Srivastava <Nileshsri2022>
2023-10-26 14:06:00 +0530

    first commit

---
+++ src/components/Random.jsx
@@ -0,0 +1,29 @@
+// import useGif from "../hooks/useGif";
+import Spinner from "./Spinner";
+import useGif from "../hooks/useGif";
+
+
+
+
+
+const Random = ( ) => {
+
+  const { gif, loading, fetchData } = useGif();
+ 
+  return (
+    <div className="w-1/2  bg-green-500 mx-auto rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+      <h1 className="  mt-[15px] text-3xl uppercase underline font-bold">A Random Gif</h1>
+      {
+      loading ? <Spinner /> : <img src={gif} width="450" alt="failed to load"/>
+      }
+      <button
+        onClick={() => fetchData()}
+        className="w-10/12 bg-white text-xl py-2 rounded-lg font-bold mb-[20px]"
+      >
+        Generate
+      </button>
+    </div>
+  );
+};
+
+export default Random;
